6ccf53503184a11c134e9617f2330666795c7636
Christine Buell <xinebuell@Christine-Buells-MacBook-Pro.local>
2014-04-15 18:54:24 -0400

    added a test class

---
+++ Git Test/CMBTest.h
@@ -0,0 +1,13 @@
+//
+//  CMBTest.h
+//  Git Test
+//
+//  Created by Christine Buell on 4/15/14.
+//  Copyright (c) 2014 Christine Buell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CMBTest : NSObject
+
+@end
---
+++ Git Test/CMBTest.m
@@ -0,0 +1,13 @@
+//
+//  CMBTest.m
+//  Git Test
+//
+//  Created by Christine Buell on 4/15/14.
+//  Copyright (c) 2014 Christine Buell. All rights reserved.
+//
+
+#import "CMBTest.h"
+
+@implementation CMBTest
+
+@end
